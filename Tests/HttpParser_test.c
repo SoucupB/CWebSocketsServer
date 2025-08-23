d0ea844bf792f7cbe3f2b9a66816f62c660c1bdc
SoucupB <bogdan.soucup@gmail.com>
2025-08-24 01:39:54 +0300

    Add one more spec

--- Tests/HttpParser_test.c
+++ Tests/HttpParser_test.c
@@ -31,6 +31,7 @@ Content-Length: 0\r\n\
   assert_memory_equal(http_GetValue(httpObj, "Host"), "space_bots_instance_1.api.com", sizeof("space_bots_instance_1.api.com") - 1);
   assert_memory_equal(http_GetValue(httpObj, "Content-Length"), "0", sizeof("0") - 1);
   assert_memory_equal(http_GetValue(httpObj, "Connection"), "keep-alive", sizeof("keep-alive") - 1);
+  assert_null(http_GetValue(httpObj, "Some-missing-key"));
   http_Delete(httpObj);
 }
 
@@ -103,6 +104,18 @@ GET /connect: HTTP/1.1\r\n\
   assert_null(httpObj);
 }
 
+static void test_http_parser_missing_missing_space(void **state) {
+  char *request = "\
+GET /connect HTTP/1.1\r\n\
+Content-Type:application/json\r\n\
+\r\n\
+";
+  PHttp httpObj = http_Parse(request, strlen(request));
+  assert_non_null(httpObj);
+  assert_memory_equal(http_GetValue(httpObj, "Content-Type"), "application/json", sizeof("application/json") - 1);
+  http_Delete(httpObj);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(test_http_parser_full_http_body),
@@ -111,6 +124,7 @@ int main(void) {
     cmocka_unit_test(test_http_parser_missing_ilegal_values),
     cmocka_unit_test(test_http_parser_missing_ilegal_keys),
     cmocka_unit_test(test_http_parser_missing_ilegal_path_chars),
+    cmocka_unit_test(test_http_parser_missing_missing_space),
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
