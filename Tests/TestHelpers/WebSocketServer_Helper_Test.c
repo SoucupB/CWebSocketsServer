c9d0fd7e807fce17a38a6dbad499c16ef377d710
SoucupB <bogdan.soucup@gmail.com>
2025-08-30 12:09:28 +0300

    Move methods in parent

---
+++ Tests/TestHelpers/WebSocketServer_Helper_Test.c
@@ -0,0 +1,24 @@
+#include "WebSocketServer_Helper_Test.h"
+#include "Socket_Helper_test.h"
+
+PConnection test_Util_ExchangeConnection(PWebSocketServer wssServer) {
+  PConnection connection = test_Util_Connect(wssServer->socketServer);
+  char *handshakeMessage = "\
+GET /chat HTTP/1.1\r\n\
+Host: example.com:80\r\n\
+Upgrade: websocket\r\n\
+Connection: Upgrade\r\n\
+Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n\
+Sec-WebSocket-Version: 13\r\n\
+\r\n";
+  test_Util_SendMessage(wssServer->socketServer, connection, handshakeMessage, strlen(handshakeMessage));
+char *handshakeAccepted = "\
+HTTP/1.1 101\r\n\
+Connection: Upgrade\r\n\
+Content-Length: 0\r\n\
+Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=\r\n\
+Upgrade: websocket\r\n\
+\r\n";
+  test_Util_Expect(connection, handshakeAccepted, strlen(handshakeAccepted));
+  return connection;
+}
---
+++ Tests/TestHelpers/WebSocketServer_Helper_Test.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "SocketClient.h"
+#include "SocketServer.h"
+#include "WebSocketServer.h"
+
+PConnection test_Util_ExchangeConnection(PWebSocketServer wssServer);
