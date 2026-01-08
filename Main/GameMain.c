615e8a11f955b1d067ce057b6581b064e7e6cd57
SoucupB <bogdan.soucup@gmail.com>
2026-01-08 22:44:40 +0200

    Remove comments

--- Main/GameMain.c
+++ Main/GameMain.c
@@ -55,21 +55,4 @@ int main()
   sock_Method_Delete(onLoginMethod);
   sock_Method_Delete(onRelease);
   sock_Method_Delete(onReceiveMethod);
-  // PHttpServer server = httpS_Create(8080);
-  // printf("Server initialized!\n");
-  // PSocketMethod onReceive = sock_Method_Create(
-  //   (void *)caller,
-  //   server
-  // );
-  // server->onReceive = onReceive;
-  // uint64_t currentTimestamp = tf_CurrentTimeMS();
-  // while (1)
-  // {
-  //   uint64_t currentTime = tf_CurrentTimeMS();
-  //   httpS_OnFrame(server, currentTime - currentTimestamp);
-  //   currentTimestamp = currentTime;
-  //   usleep(4 * 1000);
-  // }
-  // httpS_Delete(server);
-  // sock_Method_Delete(onReceive);
 }
